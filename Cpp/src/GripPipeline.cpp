4cec85a0063228b7d572b61083ebb8da38ea85dd
parsa-salimi <33131203+parsa-salimi@users.noreply.github.com>
2018-11-08 21:18:55 -0500

    Add files via upload

---
+++ Cpp/src/GripPipeline.cpp
@@ -0,0 +1,213 @@
+#include "GripPipeline.h"
+
+namespace grip {
+
+GripPipeline::GripPipeline() {
+}
+/**
+* Runs an iteration of the pipeline and updates outputs.
+*/
+void GripPipeline::Process(cv::Mat& source0){
+	//Step Resize_Image0:
+	//input
+	cv::Mat resizeImageInput = source0;
+	double resizeImageWidth = 320.0;  // default Double
+	double resizeImageHeight = 240.0;  // default Double
+	int resizeImageInterpolation = cv::INTER_CUBIC;
+	resizeImage(resizeImageInput, resizeImageWidth, resizeImageHeight, resizeImageInterpolation, this->resizeImageOutput);
+	//Step HSV_Threshold0:
+	//input
+	cv::Mat hsvThresholdInput = resizeImageOutput;
+	double hsvThresholdHue[] = {88.05754176146692, 127.7815699658703};
+	double hsvThresholdSaturation[] = {61.45683825016022, 255.0};
+	double hsvThresholdValue[] = {33.93885154518292, 255.0};
+	hsvThreshold(hsvThresholdInput, hsvThresholdHue, hsvThresholdSaturation, hsvThresholdValue, this->hsvThresholdOutput);
+	//Step CV_erode0:
+	//input
+	cv::Mat cvErodeSrc = hsvThresholdOutput;
+	cv::Mat cvErodeKernel;
+	cv::Point cvErodeAnchor(-1, -1);
+	double cvErodeIterations = 2.0;  // default Double
+    int cvErodeBordertype = cv::BORDER_CONSTANT;
+	cv::Scalar cvErodeBordervalue(-1);
+	cvErode(cvErodeSrc, cvErodeKernel, cvErodeAnchor, cvErodeIterations, cvErodeBordertype, cvErodeBordervalue, this->cvErodeOutput);
+	//Step Find_Contours0:
+	//input
+	cv::Mat findContoursInput = cvErodeOutput;
+	bool findContoursExternalOnly = false;  // default Boolean
+	findContours(findContoursInput, findContoursExternalOnly, this->findContoursOutput);
+	//Step Convex_Hulls0:
+	//input
+	std::vector<std::vector<cv::Point> > convexHullsContours = findContoursOutput;
+	convexHulls(convexHullsContours, this->convexHullsOutput);
+	//Step Filter_Contours0:
+	//input
+	std::vector<std::vector<cv::Point> > filterContoursContours = convexHullsOutput;
+	double filterContoursMinArea = 66.0;  // default Double
+	double filterContoursMinPerimeter = 0;  // default Double
+	double filterContoursMinWidth = 32.0;  // default Double
+	double filterContoursMaxWidth = 1000;  // default Double
+	double filterContoursMinHeight = 0;  // default Double
+	double filterContoursMaxHeight = 1000;  // default Double
+	double filterContoursSolidity[] = {0, 100};
+	double filterContoursMaxVertices = 1000000;  // default Double
+	double filterContoursMinVertices = 0;  // default Double
+	double filterContoursMinRatio = 0;  // default Double
+	double filterContoursMaxRatio = 1000;  // default Double
+	filterContours(filterContoursContours, filterContoursMinArea, filterContoursMinPerimeter, filterContoursMinWidth, filterContoursMaxWidth, filterContoursMinHeight, filterContoursMaxHeight, filterContoursSolidity, filterContoursMaxVertices, filterContoursMinVertices, filterContoursMinRatio, filterContoursMaxRatio, this->filterContoursOutput);
+}
+
+/**
+ * This method is a generated getter for the output of a Resize_Image.
+ * @return Mat output from Resize_Image.
+ */
+cv::Mat* GripPipeline::GetResizeImageOutput(){
+	return &(this->resizeImageOutput);
+}
+/**
+ * This method is a generated getter for the output of a HSV_Threshold.
+ * @return Mat output from HSV_Threshold.
+ */
+cv::Mat* GripPipeline::GetHsvThresholdOutput(){
+	return &(this->hsvThresholdOutput);
+}
+/**
+ * This method is a generated getter for the output of a CV_erode.
+ * @return Mat output from CV_erode.
+ */
+cv::Mat* GripPipeline::GetCvErodeOutput(){
+	return &(this->cvErodeOutput);
+}
+/**
+ * This method is a generated getter for the output of a Find_Contours.
+ * @return ContoursReport output from Find_Contours.
+ */
+std::vector<std::vector<cv::Point> >* GripPipeline::GetFindContoursOutput(){
+	return &(this->findContoursOutput);
+}
+/**
+ * This method is a generated getter for the output of a Convex_Hulls.
+ * @return ContoursReport output from Convex_Hulls.
+ */
+std::vector<std::vector<cv::Point> >* GripPipeline::GetConvexHullsOutput(){
+	return &(this->convexHullsOutput);
+}
+/**
+ * This method is a generated getter for the output of a Filter_Contours.
+ * @return ContoursReport output from Filter_Contours.
+ */
+std::vector<std::vector<cv::Point> >* GripPipeline::GetFilterContoursOutput(){
+	return &(this->filterContoursOutput);
+}
+	/**
+	 * Scales and image to an exact size.
+	 *
+	 * @param input The image on which to perform the Resize.
+	 * @param width The width of the output in pixels.
+	 * @param height The height of the output in pixels.
+	 * @param interpolation The type of interpolation.
+	 * @param output The image in which to store the output.
+	 */
+	void GripPipeline::resizeImage(cv::Mat &input, double width, double height, int interpolation, cv::Mat &output) {
+		cv::resize(input, output, cv::Size(width, height), 0.0, 0.0, interpolation);
+	}
+
+	/**
+	 * Segment an image based on hue, saturation, and value ranges.
+	 *
+	 * @param input The image on which to perform the HSL threshold.
+	 * @param hue The min and max hue.
+	 * @param sat The min and max saturation.
+	 * @param val The min and max value.
+	 * @param output The image in which to store the output.
+	 */
+	void GripPipeline::hsvThreshold(cv::Mat &input, double hue[], double sat[], double val[], cv::Mat &out) {
+		cv::cvtColor(input, out, cv::COLOR_BGR2HSV);
+		cv::inRange(out,cv::Scalar(hue[0], sat[0], val[0]), cv::Scalar(hue[1], sat[1], val[1]), out);
+	}
+
+	/**
+	 * Expands area of lower value in an image.
+	 * @param src the Image to erode.
+	 * @param kernel the kernel for erosion.
+	 * @param anchor the center of the kernel.
+	 * @param iterations the number of times to perform the erosion.
+	 * @param borderType pixel extrapolation method.
+	 * @param borderValue value to be used for a constant border.
+	 * @param dst Output Image.
+	 */
+	void GripPipeline::cvErode(cv::Mat &src, cv::Mat &kernel, cv::Point &anchor, double iterations, int borderType, cv::Scalar &borderValue, cv::Mat &dst) {
+		cv::erode(src, dst, kernel, anchor, (int)iterations, borderType, borderValue);
+	}
+
+	/**
+	 * Finds contours in an image.
+	 *
+	 * @param input The image to find contours in.
+	 * @param externalOnly if only external contours are to be found.
+	 * @param contours vector of contours to put contours in.
+	 */
+	void GripPipeline::findContours(cv::Mat &input, bool externalOnly, std::vector<std::vector<cv::Point> > &contours) {
+		std::vector<cv::Vec4i> hierarchy;
+		contours.clear();
+		int mode = externalOnly ? cv::RETR_EXTERNAL : cv::RETR_LIST;
+		int method = cv::CHAIN_APPROX_SIMPLE;
+		cv::findContours(input, contours, hierarchy, mode, method);
+	}
+
+	/**
+	 * Compute the convex hulls of contours.
+	 *
+	 * @param inputContours The contours on which to perform the operation.
+	 * @param outputContours The contours where the output will be stored.
+	 */
+	void GripPipeline::convexHulls(std::vector<std::vector<cv::Point> > &inputContours, std::vector<std::vector<cv::Point> > &outputContours) {
+		std::vector<std::vector<cv::Point> > hull (inputContours.size());
+		outputContours.clear();
+		for (size_t i = 0; i < inputContours.size(); i++ ) {
+			cv::convexHull(cv::Mat((inputContours)[i]), hull[i], false);
+		}
+		outputContours = hull;
+	}
+
+
+	/**
+	 * Filters through contours.
+	 * @param inputContours is the input vector of contours.
+	 * @param minArea is the minimum area of a contour that will be kept.
+	 * @param minPerimeter is the minimum perimeter of a contour that will be kept.
+	 * @param minWidth minimum width of a contour.
+	 * @param maxWidth maximum width.
+	 * @param minHeight minimum height.
+	 * @param maxHeight  maximimum height.
+	 * @param solidity the minimum and maximum solidity of a contour.
+	 * @param minVertexCount minimum vertex Count of the contours.
+	 * @param maxVertexCount maximum vertex Count.
+	 * @param minRatio minimum ratio of width to height.
+	 * @param maxRatio maximum ratio of width to height.
+	 * @param output vector of filtered contours.
+	 */
+	void GripPipeline::filterContours(std::vector<std::vector<cv::Point> > &inputContours, double minArea, double minPerimeter, double minWidth, double maxWidth, double minHeight, double maxHeight, double solidity[], double maxVertexCount, double minVertexCount, double minRatio, double maxRatio, std::vector<std::vector<cv::Point> > &output) {
+		std::vector<cv::Point> hull;
+		output.clear();
+		for (std::vector<cv::Point> contour: inputContours) {
+			cv::Rect bb = boundingRect(contour);
+			if (bb.width < minWidth || bb.width > maxWidth) continue;
+			if (bb.height < minHeight || bb.height > maxHeight) continue;
+			double area = cv::contourArea(contour);
+			if (area < minArea) continue;
+			if (arcLength(contour, true) < minPerimeter) continue;
+			cv::convexHull(cv::Mat(contour, true), hull);
+			double solid = 100 * area / cv::contourArea(hull);
+			if (solid < solidity[0] || solid > solidity[1]) continue;
+			if (contour.size() < minVertexCount || contour.size() > maxVertexCount)	continue;
+			double ratio = (double) bb.width / (double) bb.height;
+			if (ratio < minRatio || ratio > maxRatio) continue;
+			output.push_back(contour);
+		}
+	}
+
+
+
+} // end grip namespace
+
